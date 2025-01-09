d190e24248738e70560d76b8fc584366a991ac2c
HeytorPires <heytor.cacho29@gmail.com>
2025-01-09 00:00:51 -0300

    :construction: Express

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log('Hello Dev!');
+import express from 'express';
+import cors from 'cors';
+import routes from './shared/routes/index';
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('Escutando na Porta 3333! 😁');
+});
