1025fb8d0561193c75438397269b935142b5d162
HeytorPires <heytor.cacho29@gmail.com>
2025-01-21 20:04:33 -0300

    :sparkles: feat: Controller e Service Users

---
+++ src/modules/users/controllers/UsersController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import ListUserService from '../services/ListUserService';
+import CreateUserService from '../services/CreateUserService';
+
+export default class UsersController {
+  public async index(request: Request, response: Response) {
+    const listUser = new ListUserService();
+
+    const users = await listUser.execute();
+
+    return response.json(users);
+  }
+
+  public async create(request: Request, response: Response) {
+    const { name, email, password } = request.body;
+    const createUsers = new CreateUserService();
+
+    const user = await createUsers.execute({ name, email, password });
+
+    return response.json(user);
+  }
+}
