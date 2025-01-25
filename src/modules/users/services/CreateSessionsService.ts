3edfde9b46049d875a494e2dd69abdd8876599fb
HeytorPires <heytor.cacho29@gmail.com>
2025-01-25 01:37:29 -0300

    :construction: feat: JWT

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -3,18 +3,20 @@ import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import bcrypt from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
+import { sign } from 'jsonwebtoken';
 
 interface IRequest {
   email: string;
   password: string;
 }
 
-// interface IResponse {
-//   user: User;
-// }
+interface IResponse {
+  user: User;
+  token: string;
+}
 
 class CreateSessionsService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
     const user = await usersRepository.findByEmail(email);
 
@@ -27,8 +29,12 @@ class CreateSessionsService {
     if (!isPasswordCorrect) {
       throw new AppError('Incorrect email/password combination.', 401);
     }
-
-    return user;
+    const hashToken = '9c52f3311a73c9ccaab930b1c71d73f8';
+    const token = sign({}, hashToken, {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+    return { user, token };
   }
 }
 
